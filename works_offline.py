53d97b9aa628e4ad60a58003484719acd6963127
Max-Profi <max-profi@protonmail.com>
2020-10-08 02:54:04 +0300

    minor changes for linix and windows

--- works_offline.py
+++ works_offline.py
@@ -5,9 +5,20 @@ https://pypi.org/project/pyttsx3/
 
 pyttsx3 library looks for TTS engines pre-installed in your platform, here are the text-to-speech synthesizers that this library uses:
 
+
 Microsoft Speech API (SAPI5) on Windows
+
+List of available languages: https://support.microsoft.com/en-us/help/22805/windows-10-supported-narrator-languages-voices
+
+How to install new languages: https://support.microsoft.com/en-us/office/how-to-download-text-to-speech-languages-for-windows-10-d5a6b612-b3ae-423f-afa5-4f6caf1ec5d3
+
+
+espeak on Ubuntu
+make sure that 'espeak', 'ffmpeg', 'libespeak1' are installed
+sudo apt update && sudo apt install espeak ffmpeg libespeak1
+
+
 NSSpeechSynthesizer on macOS
-espeak on Ubuntu (install via Synaptic)
 """
 
 import pyttsx3
@@ -16,7 +27,6 @@ engine = pyttsx3.init()
 
 # get speaking rates
 rate = engine.getProperty("rate")
-# print("rate:", rate)
 engine.setProperty("rate", 200)  # 200 is default rate
 
 
@@ -24,23 +34,28 @@ engine.setProperty("rate", 200)  # 200 is default rate
 voices = engine.getProperty("voices")
 # for voice in voices:
 #     print("ID:", voice.id)
-#     print("Gender:", voice.gender)
-engine.setProperty("voice", "english")
-engine.setProperty("voice", "female")  # linux includes only male voices
+engine.setProperty(
+    "voice", "english"
+)  # for linux system; linux includes only male voices
 
-# engine.setProperty('voice', voices[0].id)  # changes voices. o for male
-# engine.setProperty('voice', voices[1].id)  # 1 for female
+# for windows 10
+# voice_id = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_RU-RU_IRINA_11.0"
+# engine.setProperty("voice", voice_id)
 
 # change volume
 volume = engine.getProperty("volume")
-engine.setProperty("volume", 1.0)  # setting up volume level  between 0 and 1
+engine.setProperty("volume", 1.0)  # setting up volume level  between 0.0 and 1.0
 
 
-with open("text.txt") as text:
+with open("text.txt", "r", encoding="utf-8") as text:
     text = text.read().replace("\n", " ")
 
     # Saving to a file
-    # On linux make sure that 'espeak', 'ffmpeg', 'libespeak1' are installed
-    # sudo apt update && sudo apt install espeak ffmpeg libespeak1
     engine.save_to_file(text, "offline.mp3")
     engine.runAndWait()
+
+
+"""
+runAndWait()
+    Blocks while processing all currently queued commands. Invokes callbacks for engine notifications appropriately. Returns when all commands queued before this call are emptied from the queue.
+"""
--- works_online.py
+++ works_online.py
@@ -1,24 +1,32 @@
 """
-gTTS (Google Text-to-Speech), a Python library and CLI tool to interface with Google Translate's text-to-speech API. Write spoken mp3 data to a file, a file-like object (bytestring) for further audio manipulation, or stdout.
+gTTS (Google Text-to-Speech) Python library and CLI tool to interface with Google Translate's text-to-speech API. It can write spoken mp3 data to a file, a file-like object (bytestring) for further audio manipulation.
 
 https://pypi.org/project/gTTS/
+
+On Windows you have to run VS Code as administrator. And it takes a long time to create mp3 file.
 """
 
-import os
+# import os
+# import gtts
 from gtts import gTTS
 
-# all available languages along with their IETF tags
-# print(gtts.lang.tts_langs())
+# # display all available languages along with their IETF tags
+# print(gtts.lang.tts_langs(tld="com"))
 
-lang = "ru"
+lang = "en"
 
-with open("text.txt", "r") as text_to_speech:
+with open("text.txt", "r", encoding="utf-8") as text_to_speech:
 
     text_to_speech = text_to_speech.read().replace("\n", " ")
 
-    output = gTTS(text=text_to_speech, lang=lang, slow=False)
+    output = gTTS(text=text_to_speech, lang=lang, slow=False, lang_check=False)
 
     output.save("online.mp3")
 
 
-os.system("start online.mp3")  # to play with default system player
+# os.system("start online.mp3")  # to play with default system player on windows
+
+
+# # for linux is recommended to use subprocess library
+# import subprocess
+# subprocess.call("smplayer online.mp3", shell=True)
